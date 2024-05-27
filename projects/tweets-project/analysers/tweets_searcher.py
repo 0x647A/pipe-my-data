d549f6408d64bf3b87d2965cabf695614305302d
Daria Czerniawko <daria@czerniawko.pl>
2024-05-27 14:46:50 +0200

    Improve search by word in location

--- projects/tweets-project/analysers/tweets_searcher.py
+++ projects/tweets-project/analysers/tweets_searcher.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, array_intersect, split, lit
+from pyspark.sql.functions import col, array_intersect, split, lit, lower
 
 class TweetsSearch:
     TEXT = "text"
@@ -10,14 +10,18 @@ class TweetsSearch:
 
     def search_by_word(self, word: str) -> DataFrame:
         # Search for tweets containing the word
-        return self.df.filter(col(self.TEXT).contains(word))
+        return self.df.filter(col(self.TEXT).contains(word)).select("text", "user_name", "user_location")
 
     def search_by_words(self, words: list) -> DataFrame:
         # Convert the list of words into an array and search for tweets containing any of the words
         words_array = lit(words)
         return self.df.withColumn("words_in_text", split(col(self.TEXT), " ")) \
-                      .filter(array_intersect(col("words_in_text"), words_array).isNotNull())
+                      .filter(array_intersect(col("words_in_text"), words_array).isNotNull()).select("text", "user_name", "user_location")
 
-    def search_by_word_and_location(self, word: str, location: str) -> DataFrame:
-        # Search for tweets containing the word and from a specific location
-        return self.df.filter(col(self.TEXT).contains(word) & col(self.USER_LOCATION).contains(location))
+    def filter_tweets(self, keyword: str, location: str) -> DataFrame:
+        # Filtering based on keyword and location
+        df_filtered = self.df.filter(
+            (lower(col(self.TEXT)).contains(keyword.lower())) &
+            (lower(col(self.USER_LOCATION)).contains(location.lower()))
+        )
+        return df_filtered.select("text", "user_name", "user_location")
